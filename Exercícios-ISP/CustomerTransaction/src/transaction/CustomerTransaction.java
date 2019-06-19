9656c2c4119b3d9d1dff86d5983ddb35508661aa
v1eira <ewersonv@gmail.com>
2019-06-18 22:43:12 -0300

    add .docx & start Exercícios-ISP

---
+++ Exercícios-ISP/CustomerTransaction/src/transaction/CustomerTransaction.java
@@ -0,0 +1,24 @@
+package transaction;
+
+public class CustomerTransaction {
+    
+    public void getName() {
+        
+    }
+
+    public void getDate() {
+        
+    }
+    
+    public void getProductBreakdown() {
+        
+    }
+    
+    public void prepareInvoice() {
+        
+    }
+    
+    public void chargeCostumer() {
+        
+    }
+}
---
+++ Exercícios-ISP/CustomerTransaction/src/transaction/Transaction.java
@@ -0,0 +1,9 @@
+package transaction;
+
+public class Transaction {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
