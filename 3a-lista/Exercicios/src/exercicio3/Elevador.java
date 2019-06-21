0d98201c147481f02056214a16e05dc837da9e83
v1eira <ewersonv@gmail.com>
2019-06-21 02:15:43 -0300

    finish Exercicio3 @ 3a-lista

---
+++ 3a-lista/Exercicios/src/exercicio3/Elevador.java
@@ -0,0 +1,50 @@
+package exercicio3;
+
+
+public class Elevador {
+    int andarAtual;
+    int totalAndaresPredio;
+    int capacidade;
+    int qtdPessoas;
+
+    public Elevador inicializa(Elevador elevador, int totalAndaresPredio, int capacidade) {
+        this.andarAtual = 0;
+        this.totalAndaresPredio = totalAndaresPredio;
+        this.capacidade = capacidade;
+        this.qtdPessoas = 0;
+        
+        return elevador;
+    }
+    
+    public void entra() {
+        if (this.qtdPessoas < this.capacidade)
+            this.qtdPessoas++;
+        else
+            System.out.println("Elevador cheio.");
+    }
+    
+    public void sai() {
+        if (this.qtdPessoas > 0)
+            this.qtdPessoas--;
+        else
+            System.out.println("Elevador vazio.");
+    }
+    
+    public void sobe() {
+        if (this.andarAtual < this.totalAndaresPredio)
+            this.andarAtual++;
+        else
+            System.out.println("Elevador já está no último andar.");
+    }
+    
+    public void desce() {
+        if (this.andarAtual > 0)
+            this.andarAtual--;
+        else
+            System.out.println("Elevador já está no térreo.");
+    }
+    
+    
+    
+    
+}
---
+++ 3a-lista/Exercicios/src/exercicio3/Exercicio3.java
@@ -0,0 +1,41 @@
+package exercicio3;
+
+public class Exercicio3 {
+    
+    public static void main(String[] args) {
+        Elevador el = new Elevador();
+        
+        el.inicializa(el, 10, 5);
+        
+        el.entra();
+        el.entra();
+        el.entra();
+        el.entra();        
+        el.entra();
+        el.entra();
+        
+        el.sobe();
+        el.sobe();
+        el.sobe();
+        el.sobe();
+        el.sobe();
+        el.sobe();
+        el.sobe();
+        
+        el.sai();
+        el.sai();
+        
+        el.desce();
+        el.desce();
+        el.desce();
+        el.desce();
+        el.desce();
+        el.desce();
+        el.desce();
+        el.desce();
+        
+        System.out.println(el.qtdPessoas);
+        System.out.println(el.andarAtual);
+        
+    }
+}
