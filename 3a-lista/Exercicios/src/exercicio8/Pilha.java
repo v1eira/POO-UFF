65d5f7f69639e79c136f9e6492a0ca4e8631f3aa
v1eira <ewersonv@gmail.com>
2019-06-23 20:51:13 -0300

    add exercicio8 @ 3a-lista

---
+++ 3a-lista/Exercicios/src/exercicio8/Pilha.java
@@ -0,0 +1,42 @@
+package exercicio8;
+
+import java.util.Random;
+import java.util.Stack;
+
+
+public class Pilha {
+    static Stack pilha = new Stack();
+    static Random numAleatorio = new Random();
+    static int num;
+    
+    public static void main(String[] args) {
+        new Thread(produtor).start();
+        new Thread(consumidor).start();
+    }
+    
+    public static int getNum() {
+        return num = numAleatorio.nextInt(1001);
+    }
+    
+    private static final Runnable produtor = new Runnable() {
+        @Override
+        public void run() {
+            for (int i=0; i<1000; i++) {
+                if (pilha.size() < 1000) {
+                    pilha.push(getNum());
+                }
+            }
+        }
+    };
+    
+    private static Runnable consumidor = new Runnable() {
+        @Override
+        public void run() {
+            for (int i=0; i<1000; i++) {
+                if (!pilha.empty())
+                    System.out.println("Consumidor pop: " + pilha.pop());
+            }
+        }
+    };
+    
+}
